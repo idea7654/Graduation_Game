2deb20e1f48e02655b652bb5be9f19d451760b1f
unknown <idea7654@naver.com>
2022-05-09 19:48:32 +0900

    add particle collision

--- Source/ChaosHunter/ChaosHunterGameMode.cpp
+++ Source/ChaosHunter/ChaosHunterGameMode.cpp
@@ -1,15 +1,14 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "ChaosHunterGameMode.h"
-#include "ChaosHunterCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 AChaosHunterGameMode::AChaosHunterGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
-	if (PlayerPawnBPClass.Class != NULL)
-	{
-		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}
+	//static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	//if (PlayerPawnBPClass.Class != NULL)
+	//{
+	//	DefaultPawnClass = PlayerPawnBPClass.Class;
+	//}
 }
